6d942a60e8bae3a9fc1e25f1267135151bab2b61
Joseph Gimenez <joseph.gimenez@snagajob.com>
2022-07-01 21:35:33 -0400

    chore: make some of the test logic more DRY

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -4,6 +4,16 @@ use std::fs;
 
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
+
 #[test]
 fn dies_no_args() -> TestResult {
     let mut cmd = Command::cargo_bin("echor")?;
@@ -21,21 +31,25 @@ fn runs() -> TestResult {
 }
 
 #[test]
+/// Test that the output is the same as the input.
+/// In particular this is a single, quoted argument
 fn hello1() -> TestResult {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile)?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.arg("Hello there").assert().success().stdout(expected);
-    Ok(())
+    run(&["Hello there"], "tests/expected/hello1.txt")
 }
 
 #[test]
+/// Test that the output is the same as the input.
+/// In particular, this is two, separate arguments
 fn hello2() -> TestResult {
-    let expected = fs::read_to_string("tests/expected/hello2.txt")?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.args(vec!["Hello", "there"])
-        .assert()
-        .success()
-        .stdout(expected);
-    Ok(())
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
